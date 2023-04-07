572d4851d2344fbefeb1c2a7a395da7bd74ac858
Alan Pereyro <alanpereyro0@gmail.com>
2023-04-06 22:19:02 -0400

    Start of project, getting data back from fetch

---
+++ Assets/script.js
@@ -0,0 +1,47 @@
+const API_KEY = "c742f6a434aba29ee3de171e2674ca24";
+const baseUrl = `https://api.openweathermap.org/data/2.5/forecast`;
+
+var form = document.querySelector("form");
+
+let cityName = document.getElementById("currentCity");
+
+let cityTemp = document.getElementById("cityTemp");
+
+const changeTempFormat = (temp) => {
+  // function that changes the format
+
+
+  // return yyour new data
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  var input = document.getElementById("searchInput").value;
+  // GET
+  fetch(`${baseUrl}?q=${input}&appid=${API_KEY}`) // Go fetch me some bread 5 grain 1 foot long and is $5  -Alan
+    .then((response) => response.json()) // Hey alan I have a response for you...
+    .then((data) => {
+      // here is what type of bread I got you...
+      console.log(data);
+
+      cityName.innerHTML = data.city.name;
+
+      cityTemp.innerHTML += data.list[0].main.temp;
+    })
+
+    .catch((err) => {
+      // Oh no, they had none ...
+      console.log("error", err);
+    });
+});
+
+/**
+ * @POST
+ * * Request being sent to a server
+ */
+
+/**
+ * @PUT
+ * * Is a request sent to a server to update
+ *
+ */
