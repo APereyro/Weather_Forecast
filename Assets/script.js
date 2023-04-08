f54fcbeafe9ef00bad79b8c2db48dd144fce78f5
Alan Pereyro <alanpereyro0@gmail.com>
2023-04-08 18:55:03 -0400

    changed temp to farenheit

--- Assets/script.js
+++ Assets/script.js
@@ -38,38 +38,38 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   var input = document.getElementById("searchInput").value;
 
-  fetch(`${baseUrl}?q=${input}&appid=${API_KEY}`)
+  fetch(`${baseUrl}?q=${input}&appid=${API_KEY}&units=imperial`)
     .then((response) => response.json()) 
     .then((data) => {
       console.log(data);
 
       cityName.innerHTML = data.city.name;
-      cityTemp.innerHTML += `${data.list[0].main.temp} &#8457`;  
+      cityTemp.innerHTML += `${data.list[0].main.temp} &#8457;`;  
       cityWind.innerHTML += `${data.list[0].wind.speed} MPH`;
       cityHumidity.innerHTML += `${data.list[0].main.humidity} %`;
       for(var i=0; i< 40;i=i+8){
         boxDay0.textContent = new Date(data.list[4].dt_txt).toLocaleDateString();
-        boxTemp0.textContent = `Temp: ${data.list[4].main.temp} F`;
+        boxTemp0.textContent = `Temp: ${data.list[4].main.temp} °F`;
         boxWind0.textContent = `Wind: ${data.list[4].wind.speed} MPH`;
         boxHumidity0.textContent = `Humidity: ${data.list[4].main.humidity} %`;
           
         boxDay8.textContent = new Date(data.list[12].dt_txt).toLocaleDateString();
-        boxTemp8.textContent = `Temp: ${data.list[12].main.temp} F`;
+        boxTemp8.textContent = `Temp: ${data.list[12].main.temp} °F`;
         boxWind8.textContent = `Wind: ${data.list[12].wind.speed} MPH`;
         boxHumidity8.textContent = `Humidity: ${data.list[12].main.humidity} %`;
 
         boxDay16.textContent = new Date(data.list[20].dt_txt).toLocaleDateString();
-        boxTemp16.textContent = `Temp: ${data.list[20].main.temp} F`;
+        boxTemp16.textContent = `Temp: ${data.list[20].main.temp} °F`;
         boxWind16.textContent = `Wind: ${data.list[20].wind.speed} MPH`;
         boxHumidity16.textContent = `Humidity: ${data.list[20].main.humidity} %`;
 
         boxDay24.textContent = new Date(data.list[28].dt_txt).toLocaleDateString();
-        boxTemp24.textContent = `Temp: ${data.list[28].main.temp} F`;
+        boxTemp24.textContent = `Temp: ${data.list[28].main.temp} °F`;
         boxWind24.textContent = `Wind: ${data.list[28].wind.speed} MPH`;
         boxHumidity24.textContent = `Humidity: ${data.list[28].main.humidity} %`;
 
         boxDay32.textContent = new Date(data.list[36].dt_txt).toLocaleDateString();
-        boxTemp32.textContent = `Temp: ${data.list[36].main.temp} F`;
+        boxTemp32.textContent = `Temp: ${data.list[36].main.temp} °F`;
         boxWind32.textContent = `Wind: ${data.list[36].wind.speed} MPH`;
         boxHumidity32.textContent = `Humidity: ${data.list[36].main.humidity} %`;
       }
